f75b2b11037412d9945c6a0086461aeeb9974fd2
Seth <seth.johnson@desiringgod.org>
2011-05-26 17:39:17 -0500

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodmart::Application.config.secret_token = '7035305d5cf109d1eaf5398967cffac3046ba91054b1247acaff3fab428f3c6e713f2b0876835f2fba2f0534f6477f7031bce015a980de4c74d27d267c610141'
