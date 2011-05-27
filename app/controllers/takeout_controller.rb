c8d0ba843ffb95d7987fab20f2998c10ea070b7c
Seth <seth.johnson@desiringgod.org>
2011-05-26 20:42:49 -0500

    added remote query controller

--- app/controllers/takeout_controller.rb
+++ app/controllers/takeout_controller.rb
@@ -1,11 +1,16 @@
 class TakeoutController < ApplicationController
   def index
-     @text = params[:q]
+        @text = params[:q]
+    if (@text != nil)
     @items =
       Food.find(
         :all, :conditions => ["UPPER(name) LIKE ?", "#{@text.upcase}%"]).sort!{
         |a,b| a.name.downcase <=> b.name.downcase
       }
+    else
+       @items =
+      Food.all
+    end
     render :layout => false
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Foodmart"
     if @title.nil?
       base_title
     else
